583476ba7435c537b2da5695645c4c414dd12a4a
SHIBLA-PP <ppshibla@gmail.com>
2022-08-17 11:14:32 +0530

    bank app template reference

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -30,37 +30,65 @@ export class LoginComponent implements OnInit {
 
   // user defined functions
 
-  accChange(event:any)
-  {
-    this.acno=event.target.value
-  }
+  // accChange(event:any)
+  // {
+  //   this.acno=event.target.value
+  // }
 
-  pswdChange(event:any)
-  {
-    this.pswd=event.target.value
-  }
+  // pswdChange(event:any)
+  // {
+  //   this.pswd=event.target.value
+  // }
 
-  login(){
+//   login(){
    
-    // fetch acno
-    var acno=this.acno
-    // fetch pswd
-    var pswd=this.pswd
-    let userDetails=this.database
-    if(acno in userDetails)
-    {
-      if(pswd==userDetails[acno]['password'])
-      {
-        alert('login successful')
-      }
-      else
-      {
-        alert('incorrect password')
-      }
-    }
-    else{
-      alert('user does not exist')
-    }
-  }
-}
+//     // fetch acno
+//     var acno=this.acno
+//     // fetch pswd
+//     var pswd=this.pswd
+//     let userDetails=this.database
+//     if(acno in userDetails)
+//     {
+//       if(pswd==userDetails[acno]['password'])
+//       {
+//         alert('login successful')
+//       }
+//       else
+//       {
+//         alert('incorrect password')
+//       }
+//     }
+//     else{
+//       alert('user does not exist')
+//     }
+//   }
 
+
+
+login(a:any,p:any){
+   
+      // fetch acno
+      var acno=a.value
+      // fetch pswd
+      var pswd=p.value
+      let userDetails=this.database
+      if(acno in userDetails)
+      {
+        if(pswd==userDetails[acno]['password'])
+        {
+          alert('login successful')
+        }
+        else
+        {
+          alert('incorrect password')
+        }
+      }
+      else{
+        alert('user does not exist')
+      }
+    }
+
+
+  }
+
+  
